2851291f00352d633d8336ca0cf1af69b4f6bf86
jalingo <lingojimmy@gmail.com>
2025-01-14 21:18:18 -0800

    player

--- source/main.py
+++ source/main.py
@@ -1,20 +1,30 @@
 import pygame
 from constants import *
+from player import Player
 
 def statusText():    
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+def drawScreen(player):
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen.fill((0, 0, 0))
+    player.draw(screen)
+    pygame.display.flip()
+
 def runGameLoop():
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
-        screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-        screen.fill((0, 0, 0))
-        pygame.display.flip()
+
+        drawScreen(player)
+        dt = clock.tick(60)
 
 def main():
     statusText()
