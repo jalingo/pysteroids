d8a7d8b0f21e04ede47003dcbaecd9d7d125fbde
jalingo <lingojimmy@gmail.com>
2025-01-20 12:25:53 -0800

    movements

--- source/main.py
+++ source/main.py
@@ -1,55 +1,49 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
-def statusText():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
-def draw(player):
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    screen.fill((0, 0, 0))
-    player.draw(screen)
-    pygame.display.flip()
-    print("\\", end="")
-
-def runGameLoop():
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    clock = pygame.time.Clock()
-    dt = 0
-
+def configureContainers():  
     updateable = pygame.sprite.Group()
-    updateable.add(player)
-
     drawable = pygame.sprite.Group()
-    drawable.add(player)
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updateable, drawable)
-    print(" -- runGameLoop")
+    Asteroid.containers = (asteroids, updateable, drawable)
+    AsteroidField.containers = updateable
+
+    return updateable, drawable
+
+def runGameLoop(updateable, drawable):
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    field = AsteroidField()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
 
     while True: # <- Game Loop
-        print("  -- loop: ", end="")
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        print("\\", end="")
         for object in updateable:
-            print("|", end="")
             object.update(dt)
 
-        for object in drawable:
-            print("/", end="")
-            draw(object)
+        screen.fill((0, 0, 0))
 
-        dt = clock.tick(60)
-        print(f"| dt: {dt}")
+        for object in drawable:
+            object.draw(screen)
+
+        pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+
 
 def main():
-    statusText()
     pygame.init()
-    runGameLoop()
+    updateable, drawable = configureContainers()
+    runGameLoop(updateable, drawable)
 
 if __name__ == "__main__":
     main()
